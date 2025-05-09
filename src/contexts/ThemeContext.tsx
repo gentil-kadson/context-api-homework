3200dfbf30fc97d0e260d4612d8dbcccfb16061b
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2025-05-09 18:59:16 -0300

    creates theme context.

---
+++ src/contexts/ThemeContext.tsx
@@ -0,0 +1,23 @@
+import { createContext, ReactNode, useState } from "react";
+
+interface ThemeContextType {
+  theme: "light" | "dark";
+  toggleTheme: () => void;
+}
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+export const ThemeProvider = ({ children }: { children: ReactNode }) => {
+  const [theme, setTheme] = useState<"light" | "dark">("light");
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export default ThemeContext;
