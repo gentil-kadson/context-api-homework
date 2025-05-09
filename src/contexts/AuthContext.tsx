82ea662049312f213d9aa67e990721daea777d7a
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2025-05-09 19:07:52 -0300

    creates AuthContext

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,31 @@
+import { createContext, ReactNode, useState } from "react";
+
+export interface User {
+  name: string;
+}
+
+interface AuthContextType {
+  user: User | null;
+  login: (name: string) => void;
+  logout: () => void;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
+
+  const login = (name: string) => {
+    setUser({ name });
+  };
+
+  const logout = () => {
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
